f0210366b3e42aba235e746fd9ec24a8d248f7e1
eyee19 <eyee305@gmail.com>
2018-11-02 10:19:59 -0700

    commented while loop

--- username.sh
+++ username.sh
@@ -9,14 +9,26 @@ echo
 echo "Enter username: "
 read USER
 
+accepted=false
+
 LEN=$(echo ${#USER})
 
-if [ $LEN -lt 3 ] || [ $LEN -gt 12 ]; then
+#while echo $USER | [ "$accepted" != "true" ];
+#do
+	if [ $LEN -lt 3 ] || [ $LEN -gt 12 ]; then
 		echo "Error: username must be at least 3 but no more than 12 characters"
-elif [[ "$USER" =~ [[:upper:]] ]]; then
+		#accepted=false
+		#read USER
+	elif [[ "$USER" =~ [[:upper:]] ]]; then
 		echo "Error: username cannot contain upper case letters"
-elif [[ $USER =~ ^[a-z0-9_]+$ ]] && [[ $USER =~ ^[a-z] ]] && [ $LEN -gt 2 ] && [ $LEN -lt 13 ]; then
+		#accepted=false
+		#read USER
+	elif [[ $USER =~ ^[a-z0-9_]+$ ]] && [[ $USER =~ ^[a-z] ]] && [ $LEN -gt 2 ] && [ $LEN -lt 13 ]; then
 		echo "Valid username, thank you $USER"
-else
+		#accepted=true
+	else
 		echo "Error: username must begin with a lower case letter and cannot have special characters (except the underscore)"
-fi
+		#accepted=false
+		#read USER
+	fi
+#done
