1b0651d058cc2b8fd46abdce218cb93c03d2aed8
eyee19 <eyee305@gmail.com>
2018-11-02 10:04:28 -0700

    conditions added

--- username.sh
+++ username.sh
@@ -1,13 +1,22 @@
 #! /bin/bash
 # username.sh
+echo "Username requirements: "
+echo
+echo "- Can only use lower case letters, digits, and the underscore character"
+echo "- Must begin with a lower case letter"
+echo "- Contain at least 3 but no more than 12 characters"
+echo
 echo "Enter username: "
 read USER
 
 LEN=$(echo ${#USER})
 
-if [ $LEN -lt 3 ]; then
-	echo "Username must be at least 3 characters"
+if [ $LEN -lt 3 ] || [ $LEN -gt 12 ]; then
+		echo "Error: username must be at least 3 but no more than 12 characters"
+elif [[ "$USER" =~ [[:upper:]] ]]; then
+		echo "Error: username cannot contain upper case letters"
+elif [[ $USER =~ ^[a-z0-9_]+$ ]] && [[ $USER =~ ^[a-z] ]] && [ $LEN -gt 2 ] && [ $LEN -lt 13 ]; then
+		echo "Valid username, thank you $USER"
 else
-	echo "Username has at least 3 characters"
+		echo "Error: username must begin with a lower case letter and cannot have special characters (except the underscore)"
 fi
-
